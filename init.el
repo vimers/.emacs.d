4a84368eca8e1089b917109e9ee81fac43c23195
king <chenxie86@gmail.com>
2021-11-06 16:51:02 +0800

    refactor: use rainbow-delimiters to identify parenthese easily

--- init.el
+++ init.el
@@ -4,13 +4,13 @@
 (add-to-list 'load-path
 	     (expand-file-name "lisp" user-emacs-directory))
 
-(require 'init-startup)
-(require 'init-elpa)
-(require 'init-ui)
-(require 'init-autocomplete)
-(require 'init-kbd)
-(require 'init-org)
-(require 'init-git)
+(require 'init-startup nil 'noerror)
+(require 'init-elpa nil 'noerror)
+(require 'init-ui nil 'noerror)
+(require 'init-autocomplete nil 'noerror)
+(require 'init-kbd nil 'noerror)
+(require 'init-org nil 'noerror)
+(require 'init-git nil 'noerror)
 (use-package restart-emacs)
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
